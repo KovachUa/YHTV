7a45c35c733edad11943e37d7413e57ecd7b6e7b
Kovach <goral@tutamail.com>
2026-07-11 12:12:01 +0300

    Clean up scratch files and scripts to keep the repository clean

--- tests/test_ydl.py
+++
@@ -1,5 +0,0 @@
-import yt_dlp
-opts = {'skip_download': True, 'writethumbnail': True, 'quiet': True}
-with yt_dlp.YoutubeDL(opts) as ydl:
-    info = ydl.extract_info("https://www.youtube.com/watch?v=BaW_jenozKc", download=True)
-    print("Downloaded info for:", info.get('title'))
